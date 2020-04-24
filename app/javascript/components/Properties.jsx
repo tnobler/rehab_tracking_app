281586c4afedca50adc0bb2c569457ffebfae50c
Tanner Noble <tannernoble@Tanners-iMac.local>
2020-04-24 16:56:09 -0500

    created Properties component and logic and wired its route

---
+++ app/javascript/components/Properties.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Properties extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      properties: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "/api/v1/properties/index";
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(response => this.setState({ properties: response }))
+      .catch(() => this.props.history.push("/"));
+  }
+
+  render() {
+    const { properties } = this.state;
+    const allProperties = properties.map((property, index) => (
+      <div key={index} className="col-md-6 col-lg-4">
+        <div className="card mb-4">
+          <img
+            src={property.image}
+            className="card-img-top"
+            alt={`${property.name} image`}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{property.name}</h5>
+            <Link to={`/recipe/${recipe.id}`} className="btn cusom-button">
+              View Property
+            </Link>
+          </div>
+        </div>
+      </div>
+    ));
+    const noProperty = (
+      <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
+        <h4>
+          No properties yet. Why not <Link to="/new_property">create one</Link>
+        </h4>
+      </div>
+    );
+
+    return (
+      <>
+        <section className="jumbotron jumbotron-fluid text-center">
+          <div className="container py-5">
+            <h1 className="display-4">Properties</h1>
+            <p>Here is a list of all the properties</p>
+          </div>
+        </section>
+        <div className="py-5">
+          <main className="container">
+            <div className="text-right mb-3">
+              <link to="/property" className="btn custom-button">
+                Create New Property
+              </link>
+            </div>
+            <div className="row">
+              {properties.length > 0 ? allProperties : noProperty}
+            </div>
+            <Link to="/" className="btn btn-link">
+              Home
+            </Link>
+          </main>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Properties;
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
+import Properties from "../components/Properties";
 
 export default (
   <Router>
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/properties" exact component={Properties} />
     </Switch>
   </Router>
 );
