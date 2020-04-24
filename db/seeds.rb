d232f83aded17077a7e8cfed27d38df63a861de8
Tanner Noble <tannernoble@Tanners-iMac.local>
2020-04-24 16:31:07 -0500

    seeded the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+9.times do |i|
+  Property.create(
+    name: "Property #{i + 1}",
+    units: "Unit 1, Unit 2, Unit 3, Unit 4, Unit 5, Unit 6, Unit 7, Unit 8, Unit 9, Unit 10",
+    description: "This Property has 4 units left to rehab.",
+  )
+end
